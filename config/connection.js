aa3adff87f9eb86f23b1568a4ac74888051165f6
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-05-04 00:01:18 -0400

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -33,11 +33,11 @@ const mysql = require("mysql");
  */
 const db = process.env.NODE_ENV === 'production'
             ? process.env.JAWSDB_URL
-            : { host: process.env.MYSQL_HOST,
-                port: process.env.MYSQL_PORT,
-                user: process.env.MYSQL_USER,
-                password: process.env.MYSQL_PASSWORD,
-                database: process.env.MYSQL_DATABASE };
+            : { host: "localhost",
+                port:"3306",
+                user:"root",
+                password:"",
+                database:"burgers_db" };
 
 /**
  * Call mysql.createConnection using db options and save to connection variable for export.
